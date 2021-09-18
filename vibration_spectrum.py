e7c3cf823b337b9451548f24468a5038957478da
Sanskruti Admane <admane.1@osu.edu>
2021-09-18 15:33:42 -0400

    Unit conversion for nexus lab- velocity/frequency

--- vibration_spectrum.py
+++ vibration_spectrum.py
@@ -4,9 +4,9 @@ import numpy as np
 
 ##test comment##
 
-filename = '/Users/sanskruti/Desktop/tOSU tings/summer21/Vibration Measurements Day 2/20210629-0001_PRB155_center/20210629-0001_PRB155_center_05.csv'
-filename2 = '/Users/sanskruti/Desktop/tOSU tings/summer21/Vibration Measurements Day 2/20210629-0001_PRB105_center/20210629-0001_PRB105_center_05.csv'
-filename3 = '/Users/sanskruti/Desktop/tOSU tings/summer21/Vibration Measurements Day 2/20210629-0001_PRB101_center/20210629-0001_PRB101_center_5.csv'
+filename = '/Users/sanskruti/Downloads/OneDrive_2_9-17-2021/20210914-0001-slab5/20210914-0001-slab5_1.csv'
+filename2 = '/Users/sanskruti/Downloads/OneDrive_2_9-17-2021/20210914-0001-slab5/20210914-0001-slab5_2.csv'
+filename3 = '/Users/sanskruti/Downloads/OneDrive_2_9-17-2021/20210914-0001-slab5/20210914-0001-slab5_3.csv'
 df = pd.read_csv(filename , skiprows=3, names=['Hz', 'dBv'])
 df2 = pd.read_csv(filename2, skiprows=3, names=['Hz', 'dBv'])
 df3 = pd.read_csv(filename3, skiprows=3, names=['Hz', 'dBv'])
@@ -21,44 +21,52 @@ df['acc'] = df['v_m'].apply(lambda x: x/100 * 9.81)
 df2['acc'] = df2['v_m'].apply(lambda x: x/100 * 9.81)
 df3['acc'] = df3['v_m'].apply(lambda x: x/100 * 9.81)
 
-#convert acc to displacement (um)
-df['dis'] = (0.5 * df['acc'] * (1/df['Hz']) ** 2) * 10**6
-df['log'] = np.log10(df['dis'])
+#convert acc to velocity (microns/seconds)
+df['dis'] = (0.5 * df['acc'] * (1/df['Hz'])) * 10**6
+#df['log'] = np.log10(df['dis'])
 
-df2['dis'] = (0.5 * df2['acc'] * (1/df['Hz']) ** 2) * 10**6
-df2['log'] = np.log10(df2['dis'])
+df2['dis'] = (0.5 * df2['acc'] * (1/df['Hz'])) * 10**6
+#df2['log'] = np.log10(df2['dis'])
 
-df3['dis'] = (0.5 * df3['acc'] * (1/df['Hz']) ** 2) * 10**6
+df3['dis'] = (0.5 * df3['acc'] * (1/df['Hz'])) * 10**6
 df3['log'] = np.log10(df3['dis'])
+
+#convert Hz to seconds
+df['seconds'] = (1/df['Hz'])
+df2['seconds'] = (1/df['Hz'])
+df3['seconds'] = (1/df['Hz'])
 
 # Hz = list(df['Hz'])
 # dis = list(df['dis'])
 
 #plots data
 
-#plt.plot(list(df['Hz']), list(df['dis']), linewidth=0.5, label = 'PRB155 0629')
-#plt.plot(list(df2['Hz']), list(df2['dis']), linewidth=0.5, label = 'PRB105 0629')
-#plt.plot(list(df3['Hz']), list(df3['dis']), linewidth=0.5, label = 'PRB101 0629')
+plt.plot(list(df['Hz']), list(df['dis']), linewidth=0.5, label = '1')
+plt.plot(list(df2['Hz']), list(df2['dis']), linewidth=0.5, label = '2')
+plt.plot(list(df3['Hz']), list(df3['dis']), linewidth=0.5, label = '3')
+
 
 #smoothed data
 windowSize = 10
 
-series = type(df.squeeze())
+series = df.squeeze()
+#print(type(series))
 print(series)
-rolling = series.rolling(10,1)
+rolling = series.rolling(windowSize)
 mean = rolling.mean()
 print(mean)
-sdList = mean.tolist()
+sdList = mean.values.tolist()
 sdNew = sdList[windowSize - 1]
 
-plt.plot(sdNew, linewidth = 2, label = 'Smooth')
+#plt.plot(sdNew, linewidth = 2, label = 'Smooth')
 
 plt.xlabel('Hz')
-plt.ylabel('Displacement, um')
+plt.ylabel('Velocity, um/seconds')
 plt.yscale('log')
-plt.ylim([10**(-7), 10])
+plt.ylim([10**(-4), 10])
+plt.xlim([0, 400])
 plt.grid(linestyle='-')
 plt.legend(loc = 'best')
-plt.title('Vibration Spectrum')
+plt.title('Vibration Spectrum- Center Tuesday')
 # plt.savefig(r'filepath' + filename + '.png', dpi=200)
 plt.show()
